a3bba7c4c883285d3b1d419a99e4ad40b8767160
João Vitor <joaosaraivasapori@gmail.com>
2025-12-09 20:41:04 -0300

    crud codevasf dto

--- backend/src/processes-codevasf/dto/create-processes-codevasf.dto.ts
+++ backend/src/processes-codevasf/dto/create-processes-codevasf.dto.ts
@@ -1,1 +1,65 @@
-export class CreateProcessesCodevasfDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { ProcessCodevasfStatus } from 'common/enums/process-codevasf-status.enum';
+
+export class CreateProcessesCodevasfDto {
+  @ApiProperty({
+    example: 'Entrega de Trator - Emenda Dep. Fulano',
+    description:
+      'Título curto para identificação fácil. Se vazio, será gerado automaticamente.',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @ApiProperty({ example: 'Doação de Trator para Associação X' })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({ example: 150000.0, description: 'Valor do bem ou recurso' })
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsNotEmpty()
+  value: number;
+
+  // --- AMARRAÇÃO COM OS MÓDULOS QUE CRIAMOS ---
+
+  @ApiProperty({
+    description: 'ID do Bem (Good) a ser doado',
+    example: 'uuid-do-bem',
+  })
+  @IsUUID(4, { message: 'ID do bem inválido' })
+  @IsNotEmpty()
+  goodId: string;
+
+  @ApiProperty({
+    description: 'ID do Beneficiário (Quem recebe)',
+    example: 'uuid-do-beneficiario',
+  })
+  @IsUUID(4, { message: 'ID do beneficiário inválido' })
+  @IsNotEmpty()
+  beneficiaryId: string;
+
+  @ApiProperty({
+    description: 'ID do Apoiador (Quem indicou)',
+    example: 'uuid-do-apoiador',
+    required: false,
+  })
+  @IsOptional()
+  @IsUUID(4, { message: 'ID do apoiador inválido' })
+  supporterId?: string;
+
+  // Status inicial opcional (geralmente começa como TRIAGEM, mas pode-se forçar outro)
+  @ApiProperty({ enum: ProcessCodevasfStatus, required: false })
+  @IsOptional()
+  @IsEnum(ProcessCodevasfStatus)
+  status?: ProcessCodevasfStatus;
+}
