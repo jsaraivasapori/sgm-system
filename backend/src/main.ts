0f559f0cf7325c1f8657da430f4f16d6148d4a63
João Vitor <joaosaraivasapori@gmail.com>
2025-12-09 22:47:42 -0300

    feat(backend): configuração de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://34.122.109.191'], // O endereço exato do seu Frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Importante se você usa Cookies ou Headers de Autorização
+  });
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
